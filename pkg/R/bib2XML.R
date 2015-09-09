007f5d53fe74bb8d012efe6974cea43884578f38
kmm <kmm@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-09-09 04:51:46 +0000

    changes to bib2XML
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jss@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bib2XML.R
+++ pkg/R/bib2XML.R
@@ -65,24 +65,63 @@ get_authors <- function(x) {
     }
     str
 }
-get_doi <- function(x) {
+get_doi <- function(x, type) {
     prefix <- "10.18637/"
-    if(nchar(x$volume) == 2)
-        v <- "v0"
-    else v <- "v" 
-    suffix <- paste("jss.", v, x$volume, ".i", x$number, sep="")
+    v <- switch(nchar(x$volume), "v00", "v0", "v")
+    i <- switch(as.character(type), article = ".i", codesnippet = ".c",
+                bookreview = ".b", softwarereview = ".s",
+                stop("type should be: article, codesnippet, bookreview or softwarereview"))
+    n <- switch(nchar(x$number), "00", "0", "")
+    if(is.null(v) || is.null(n)) stop("Problem with spec. of vol. or issue.")
+    suffix <- paste("jss.", v, x$volume, i, n, sep="")
     paste(prefix,suffix, sep="")
 }
-strip_title <- function(x) {
-    x <- gsub("\\\\proglang", "", x)
-    x <- gsub("\\\\pkg", "", x)
-    x <- gsub("\\\\code", "", x)
-    return(x)
+htmlify <- function(x, collapse = "\n") {
+      tab <- rbind(
+        c("\\proglang", "\\textsf"),
+        c("\\pkg", "\\textbf"),
+        c("\\code", "\\texttt"),
+        c("\\bm", "")
+    )
+    for(i in 1:nrow(tab)) {
+        x <- gsub(tab[i,1], tab[i,2], x, fixed = TRUE)
+    }
+    x <- tth::ttm(x, mode="hex")
+    if(is.character(collapse)) x <- paste(x, collapse = collapse)
+    if(length(grep("<math", x)) > 0) {
+        xx <- strsplit(x, "<math")[[1]]
+        xx[1] <- paste(xx[1], "<mml:math")
+        for(i in 2:length(xx)) {
+            xL <- strsplit(xx[i], "</math>")
+            if(length(xL[[1]]) == 2)  {# there is something after the mathml
+                p1 <- xL[[1]][1]
+                p2 <- paste("</mml:math>", xL[[1]][2])
+            }
+            else {
+                p1 <- xx[i]
+                p2 <- ""
+            }
+            p1 <- gsub("</", "!!!/mml:", p1)
+            p1 <- gsub("<", "<mml:", p1)
+            p1 <- gsub("!!!", "<", p1)
+            xx[i] <- paste(p1, p2)
+            if(i < length(xx))
+                xx[i] <- paste(xx[i], "<mml:math")
+            
+        }
+    }
+    x <- paste(xx, collapse=" ")
+    ## need to remove all <font face="helvetica">; parser chokes 
+    x <- gsub("<font face=\"helvetica\">", "<i>", x, fixed = TRUE)
+    x <- gsub("</font>", "</i>", x, fixed = TRUE)
+
+    return(x) 
 }
-print_journal_article <- function(x, file) {
+
+print_journal_article <- function(x, type, file) {
     cat(paste("<journal_article publication_type=\"full_text\"> \n
 				<titles> \n
-					<title>", strip_title(x$title),
+				<title>", htmlify(x$title),
               "</title> \n
 				</titles> \n
 				<contributors> \n", get_authors(x), 
@@ -91,10 +130,11 @@ print_journal_article <- function(x, file) {
 					<year>", x$year,"</year>\n 
 				</publication_date> \n
 				<publisher_item>
-					<identifier id_type=\"doi\">", get_doi(x), "</identifier> \n
+					<identifier id_type=\"doi\">",
+              get_doi(x, type), "</identifier> \n
 				</publisher_item> \n
 				<doi_data>\n
-					<doi>", get_doi(x),"</doi> \n
+					<doi>", get_doi(x, type),"</doi> \n
 					<resource>", x$url,"</resource>\n
 				</doi_data>\n
 			</journal_article>\n", sep=""), file=file, append=TRUE)
@@ -102,9 +142,10 @@ print_journal_article <- function(x, file) {
 newDeposit <- function(file, type="article", out="out.xml") {
     ## possible types are bookreview, article, codesnippet, softwarereview
     x <- bibtex::read.bib(file)
-    cat("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n
-<doi_batch version=\"4.3.4\" xmlns=\"http://www.crossref.org/schema/4.3.4\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.crossref.org/schema/4.3.4 http://www.crossref.org/schema/deposit/crossref4.3.4.xsd\"> \n
+cat("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n
+<doi_batch version=\"4.3.4\" xmlns=\"http://www.crossref.org/schema/4.3.4\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.crossref.org/schema/4.3.4 http://www.crossref.org/schema/deposit/crossref4.3.4.xsd\"  xmlns:mml=\"http://www.w3.org/1998/Math/MathML\"> \n
 <head>", file=out)
+    
     print_doi_batch_id(out)
     print_timestamp(out)
     print_depositor(out)
@@ -117,13 +158,13 @@ newDeposit <- function(file, type="article", out="out.xml") {
     ## this generates xml for all bib items stored in a bib file
     ## do not put multiple journal titles in one bib file for deposit
     for(i in 1:length(x)) 
-        print_journal_article(x[[i]], out)
+        print_journal_article(x[[i]], type, out)
     cat("</journal> \n
 	</body> \n 
        </doi_batch>", file=out, append=TRUE)
-    ## once we have a password, login we can test via
+    ## test via
     ## http://test.crossref.org, see http://help.crossref.org/verifying_your_xml
     ## make real deposits with https://doi.crossref.org/servlet/deposit
-    system(paste("curl -F \'operation=doMDUpload\' -F \'login_id=fast\' -F \'login_passwd=fast_827\' -F \'fname=@", out, "\' ", "https://test.crossref.org/servlet/deposit", sep=""))
+    #system(paste("curl -F \'operation=doMDUpload\' -F \'login_id=fast\' -F \'login_passwd=fast_827\' -F \'fname=@", out, "\' ", "https://test.crossref.org/servlet/deposit", sep=""))
                        
 }
