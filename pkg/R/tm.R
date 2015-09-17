dff67172c24324b7371dd561b7a5d62ab83ec796
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-09-17 14:55:40 +0000

    read abstract from JSS PDF files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jss@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/tm.R
+++ pkg/R/tm.R
@@ -3,12 +3,30 @@ jssabstract <- function(file = Sys.glob("v*.pdf")) {
   if(identical(Sys.getlocale(), "C")) {
     Sys.setlocale("LC_ALL", "en_US.UTF-8")
   }
+
+  ## use tm to read PDF
   x <- as.character(tm::Corpus(tm::URISource(file), readerControl = list(reader = tm::readPDF))[[1L]])
+
+  ## extract abstract
   st <- which(x == "Abstract")[1L] + 1L
   en <- which(substr(x, 1L, 9L) == "Keywords:") - 1L
   while(x[st] == "") st <- st + 1L
   while(x[en] == "") en <- en - 1L
   x <- paste(x[st:en], collapse = " ")
-  ## FIXME: fixup quotes, hyphens, etc.
+
+  ## fixup UTF-8 quotes and hyphens
+  fix <- rbind(
+    c("\303\227", "x"),
+    c("\342\210\222", "-"),
+    c("\342\200\223", " - "),
+    c("\342\200\224", " - "),
+    c("\342\200\230", "'"),
+    c("\342\200\231", "'"),
+    c("\342\200\234", "\""),
+    c("\342\200\235", "\"")
+  )
+  for(i in 1:nrow(fix)) x <- gsub(fix[i, 1], fix[i, 2], x, fixed = TRUE)
+
   return(x)
 }
+
